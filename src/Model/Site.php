183c94e0d051efe15bd68d5ae296aeb7d0d4128b
Tynor Fujimoto <tynorf@gmail.com>
2018-09-09 18:01:58 -0700

    Add tests for site model

--- src/Model/Site.php
+++ src/Model/Site.php
@@ -1,8 +1,28 @@
 <?php
 namespace Lockr\Model;
 
+use DateTime;
+
 class Site extends ModelBase
 {
+    /**
+     * @returns DateTime
+     */
+    public function getCreated()
+    {
+        $created = $this->getAttribute('created');
+        return DateTime::createFromFormat(DateTime::RFC3339, $created);
+    }
+
+    /**
+     * @returns DateTime
+     */
+    public function getModified()
+    {
+        $modified = $this->getAttribute('modified');
+        return DateTime::createFromFormat(DateTime::RFC3339, $modified);
+    }
+
     /**
      * @returns string
      */
