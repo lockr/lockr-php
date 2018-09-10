5cb20f79ca71591100100dc3eda9ed626e5ef376
Tynor Fujimoto <tynorf@gmail.com>
2018-09-09 18:06:55 -0700

    Add tests to client token model

--- src/Model/ClientToken.php
+++ src/Model/ClientToken.php
@@ -1,6 +1,34 @@
 <?php
 namespace Lockr\Model;
 
+use DateTime;
+
 class ClientToken extends ModelBase
 {
+    /**
+     * @returns DateTime
+     */
+    public function getCreated()
+    {
+        $created = $this->getAttribute('created');
+        return DateTime::createFromFormat(DateTime::RFC3339, $created);
+    }
+
+    /**
+     * @returns string
+     */
+    public function getEnv()
+    {
+        return $this->getAttribute('env');
+    }
+
+    /**
+     * @returns array
+     */
+    public function getSite()
+    {
+        return $this->getRelationship('site');
+    }
 }
+
+// ex: ts=4 sts=4 sw=4 et:
