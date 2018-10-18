e3fc0ecfebf3cc0cfc7ef2268ca9fa5cf2e69061
Tynor Fujimoto <tynorf@gmail.com>
2018-10-18 15:35:39 -0700

    Add dev settings

---
+++ src/DevSettings.php
@@ -0,0 +1,38 @@
+<?php
+namespace Lockr;
+
+class DevSettings extends AnonSettings
+{
+    /** @var string $certPath */
+    private $certPath;
+
+    /**
+     * @param string|null $cert_path
+     */
+    public function __construct($cert_path = null)
+    {
+        $this->certPath = $cert_path;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getHostname()
+    {
+        return 'host.docker.internal:8443';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOptions()
+    {
+        $options = ['verify' => false];
+        if ($this->certPath) {
+            $options['cert'] = $this->certPath;
+        }
+        return $options;
+    }
+}
+
+// ex: ts=4 sts=4 sw=4 et:
