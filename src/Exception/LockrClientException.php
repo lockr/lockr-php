ed37fa636c69a1d9ff018b665a4e7f90bfa8b936
Tynor Fujimoto <tynorf@gmail.com>
2019-01-23 19:20:23 -0800

    Remove unused exceptions

--- src/Exception/LockrClientException.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Lockr\Exception;
-
-class LockrClientException extends LockrApiException
-{
-}
-
-// ex: ts=4 sts=4 sw=4 et:
--- src/Exception/LockrServerException.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Lockr\Exception;
-
-class LockrServerException extends LockrApiException
-{
-}
-
-// ex: ts=4 sts=4 sw=4 et:
