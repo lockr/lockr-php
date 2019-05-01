fc76102f8bae0efc478c4bf9eea1869d5de79b32
Tynor Fujimoto <tynorf@gmail.com>
2019-05-01 11:07:07 -0700

    Update version constant

--- src/LockrClient.php
+++ src/LockrClient.php
@@ -8,7 +8,7 @@ use Lockr\Exception\LockrApiException;
 
 class LockrClient
 {
-    const VERSION = '1.0.1';
+    const VERSION = '1.0.2';
 
     /** @var GuzzleHttp\ClientInterface $httpClient */
     private $httpClient;
