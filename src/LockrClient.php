8f5077580950b10bec290bd57eae352275332c72
Tynor Fujimoto <tynor@lockr.io>
2020-06-25 12:21:18 -0700

    Bump version

--- src/LockrClient.php
+++ src/LockrClient.php
@@ -8,7 +8,7 @@ use Lockr\Exception\LockrApiException;
 
 class LockrClient
 {
-    const VERSION = '1.0.9';
+    const VERSION = '1.0.11';
 
     /** @var GuzzleHttp\ClientInterface $httpClient */
     private $httpClient;
